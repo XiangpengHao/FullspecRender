65c7cd5237c21e47c0bbf0965531563b53eb9184
Xiangpeng Hao <3150102255@zju.edu.cn>
2018-08-20 14:51:32 -0700

    :art: refactor postprocessor

--- PostProcessor.py
+++ PostProcessor.py
@@ -3,6 +3,10 @@ import CONSTANT
 from typing import Dict
 import numpy as np
 from PIL import Image
+from ColorSpace import Spectrum, XYZ, RGB
+import warnings
+import os
+import argparse
 
 
 class PostProcessor:
@@ -33,3 +37,43 @@ class PostProcessor:
     for key in sorted_waves[1:]:
       spectrum = np.append(spectrum, wave_length_map[key], axis=2)
     self.spectrum = spectrum
+  
+  def output_as_srgb(self, output: str = None):
+    if self.spectrum is None:
+      raise ValueError("Spectrum is none")
+    xyz_image = np.zeros((*self.img_shape, 3), dtype=np.double)
+    for i in range(self.img_shape[0]):
+      for j in range(self.img_shape[1]):
+        xyz = Spectrum.spec_to_xyz(self.spectrum[i, j, :])
+        xyz_image[i, j, :] = xyz.np_xyz
+    max_xyz = np.amax(xyz_image)
+    xyz_image /= max_xyz
+    
+    rgb_image = np.zeros((*self.img_shape, 3), dtype=np.uint8)
+    for i in range(self.img_shape[0]):
+      for j in range(self.img_shape[1]):
+        rgb = XYZ(*xyz_image[i, j, :]).to_srgb()
+        rgb_image[i, j, :] = rgb.to_uint8()
+    img = Image.fromarray(rgb_image, "RGB")
+    
+    if output is None:
+      dir_path, base_name = os.path.split(self.folder)
+      output = dir_path[:-1] + "_output.png"
+    img.save(output)
+
+
+def arg_parse():
+  parser = argparse.ArgumentParser(description="Compose a set of source images to full spectrum images")
+  parser.add_argument("-i", "--input", required=True)
+  parser.add_argument("-o", "--output", required=False)
+  args = vars(parser.parse_args())
+  input_path = args["input"]
+  if not os.path.isdir(input_path):
+    raise ValueError("input should be a directory")
+  processor = PostProcessor(input_path)
+  processor.compose()
+  processor.output_as_srgb()
+
+
+if __name__ == "__main__":
+  arg_parse()
