f9410d7eb0801c6edf5f28305f654793bb043966
Xiangpeng Hao <3150102255@zju.edu.cn>
2018-11-01 23:41:24 -0700

    Add draft support for exr file

--- PostProcessor.py
+++ PostProcessor.py
@@ -9,9 +9,12 @@ import os
 import logging
 import argparse
 import utils
+import OpenEXR, Imath
 from joblib import Parallel, delayed
 
 logger = logging.getLogger(__name__)
+
+CHANNELS=["Composite.Combined.R","Composite.Combined.G","Composite.Combined.B"]
 
 class PostProcessor:
   def __init__(self, folder: str):
@@ -21,9 +24,59 @@ class PostProcessor:
     
     all_files = os.listdir(folder)
     # The image should be named like _400_405_410_nm.png
-    self.all_images = [x for x in all_files if x.endswith("nm.png") and not x.startswith('.')]
+    self.all_images = [x for x in all_files if x.startswith("_")]
+
+    if self.all_images[0].endswith('.png'):
+      self.file_type='png'
+    elif self.all_images[0].endswith('.exr'):
+      self.file_type='exr'
+
     if len(self.all_images) != CONSTANT.SPEC_LENGTH // 3:
       raise AssertionError(f'Invalid spectrum folder: {folder}')
+
+  def png_data_loader(self):
+    wave_length_map: Dict[int, np.ndarray] = {}
+    for f in self.all_images:
+      waves = [int(x) for x in f.split("_")[1:4]]
+      tmp_img: np.ndarray = np.array(Image.open(os.path.join(self.folder, f))) / 255
+      if self.img_shape is None:
+        self.img_shape = tmp_img.shape[:-1]
+      elif self.img_shape != tmp_img.shape[:-1]:
+        raise ArithmeticError("The image shapes don't match")
+      wave_length_map[waves[0]] = tmp_img[:, :, 0].reshape((*self.img_shape, 1))
+      wave_length_map[waves[1]] = tmp_img[:, :, 1].reshape((*self.img_shape, 1))
+      wave_length_map[waves[2]] = tmp_img[:, :, 2].reshape((*self.img_shape, 1))
+    sorted_waves = sorted(wave_length_map.keys())
+    spectrum = wave_length_map[sorted_waves[0]]
+    for key in sorted_waves[1:]:
+      spectrum = np.append(spectrum, wave_length_map[key], axis=2)
+    # interpolate the spectrum from 60 values to 61 values
+    spectrum = np.append(spectrum, wave_length_map[sorted_waves[-1]], axis=2)
+    
+    self.spectrum = spectrum
+  
+  def exr_data_loader(self):
+    wave_length_map:Dict[int,np.ndarray] = {}
+    pixel_type=Imath.PixelType(Imath.PixelType.HALF)
+    for f in self.all_images:
+      waves = [int(x) for x in f.split("_")[1:4]]
+      tmp_img=OpenEXR.InputFile(os.path.join(self.folder,f))
+      if self.img_shape is None:
+        dw = tmp_img.header()['dataWindow']
+        self.img_shape=(dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+      for i,w in enumerate(waves):
+        buffed=tmp_img.channel(CHANNELS[i],pixel_type)
+        channel_data=np.frombuffer(buffed,dtype=np.float16).astype(np.float32)
+        channel_data.shape=self.img_shape
+        wave_length_map[w]=channel_data.reshape((*self.img_shape,1))
+    sorted_waves=sorted(wave_length_map.keys())
+    spectrum=wave_length_map[sorted_waves[0]]
+    for key in sorted_waves[1:]:
+      spectrum=np.append(spectrum, wave_length_map[key], axis=2)
+    spectrum=np.append(spectrum, wave_length_map[sorted_waves[-1],axis=2])
+    self.spectrum=spectrum
+
+
   
   def compose(self):
     wave_length_map: Dict[int, np.ndarray] = {}
